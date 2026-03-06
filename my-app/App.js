9a648842b75f171139597dd5aff2080beb898d55
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-03-06 11:52:12 +0000

    final aula-02

--- my-app/App.js
+++ my-app/App.js
@@ -4,17 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <view style={styles.bloco}>
+        <Text style={styles.texto}>Mobilia Tietê!</Text> 
       <StatusBar style="auto" />
+      </view>
+      <view style={styles.bloco}>
+        <Text style={styles.texto}>A melhor loja de móveis da região!</Text> 
+        
+      </view>
+        <view style={styles.bloco}>
+        <Text style={styles.texto}>A melhor loja de móveis da região!</Text>
+        </view>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    backgroundColor: '#473e3eff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+  },  
+  
+  bloco:{
+    backgroundColor: 'white',
+    padding: 20,
+    width: '100%',   
+},
+  texto:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5f4940ff',
   },
 });
