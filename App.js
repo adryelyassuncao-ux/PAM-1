68ce0078f139ee783faa874cf00ce6089f5f8819
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-03-14 22:16:36 +0000

    final aula-04

--- App.js
+++ App.js
@@ -1,59 +1,79 @@
 import { Image } from 'expo-image';
 import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function App() {
+  const size_icons = 38
+  const color_icons = "#fff"
   return (
-  <View style={estilo.container}>
-   <View style={estilo.card}>
- <Image
-        style={estilo.imagem}
-        source="https://pngimg.com/uploads/simpsons/simpsons_PNG16.png"
-      />
-   </View>
-   <View style={estilo.infos}>
+    <View style={estilos.container}>
+         <View style={estilos.card}>
+           <Image
+             style={estilos.imagem}
+             source="https://img.icons8.com/doodle/1200/bart-simpson.jpg"        
+          />
+      </View>
+         <View style={estilos.infos}>
+      <View style={estilos.social}>
+           <FontAwesome5 name="facebook" size={size_icons} color= {color_icons}/>
+           <FontAwesome5 name="instagram"size={size_icons} color= {color_icons}/>
+           <FontAwesome5 name="whatsapp" size={size_icons} color= {color_icons}/>
+           <FontAwesome5 name="github"   size={size_icons} color= {color_icons}/>
+           <FontAwesome5 name="tiktok"   size={size_icons} color= {color_icons}/>
+      </View>
+      </View>
 
-   </View>
-     
+      <View style={estilos.fotos}>
+        <Image
+        style={estilos.retrato}
+        src="imagem"
+       />
+      </View>
+       
 
     </View>
+     
   );
 }
 
-    const estilo = StyleSheet.create({
-      container:{
-        flex:1,
-        backgroundColor:"#58aacaff",
-        alignItems: "center",
+const estilos = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#c5c5c5ff',
+    alignItems: 'center',
+    paddingTop: 10
+  },
+  card:{
+    width: 250,
+    height: 250,
+    borderColor: '#000',
+    borderWidth: 5,
+    borderStyle: 'solid',
+    borderRadius: '50%',
+    padding:10,
+    zIndex:1
+  },
+  imagem:{
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%'
+  },
+  infos:{
+    width: '90%',
+    backgroundColor: '#807e7e',
+    marginTop: -125,
+    zIndex: 0
+  },
+  social:{
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  marginTop: 125,
+  padding:20
+  },
+  retrato:{
+  width:300,
+  height:700,
+  marginTop:30
+  }
 
-      },
-
-      imagem: {
-        width: 200,
-        height: 200,
-        borderRadius: "50%",
-        borderColor: "#000",
-        borderWidth: "50%"
-        
-      },
-
-      card:{
-      width: 80,
-      height:80,
-      backgroundColor: "#fff",
-      borderWidth: 5,       
-      borderRadius: "50%",
-      borderStyle: 'solid',
-      padding: 10
-      }
-    });
-
-
-
-
-
-  
-      
-      
-
-
-
+});
